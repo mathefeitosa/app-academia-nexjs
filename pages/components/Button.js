5857e6145d30be6d0b39f141f91e0833b7dda161
Matheus Feitosa <mathe.feitosa@gmail.com>
2021-05-30 18:39:03 -0300

    Should I change the data structure?

--- pages/components/Button.js
+++ pages/components/Button.js
@@ -1,13 +1,12 @@
-function Button(props) {
+function Button({ workout }) {
   return (
     <div
-      key={props.workout.id}
       onClick={() => {
-        props.setSelectedWorkoutID(props.workout.id);
+        //props.setSelectedWorkoutID(props.workout.id);
       }}
       className="flex-initial p-4 rounded-xl bg-gray-500 hover:bg-gray-400 transform ease-in hover:scale-105 transition duration-100 font-bold text-gray-100 hover:shadow-lg"
     >
-      {props.workout.letter}
+      {console.log(workout)}
     </div>
   );
 }
